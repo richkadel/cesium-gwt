040d3e347b0d6e6f8fa99029100ab3c13c7a2e17
Mark Erikson <mark.erikson@ngc.com>
2014-09-02 17:03:21 -0400

    Add/update Scene, SkyAtmosphere, and SkyBox wrappers

---
+++ src/main/java/org/cesiumjs/cesium/SkyAtmosphere.java
@@ -0,0 +1,10 @@
+package org.cesiumjs.cesium;
+
+import com.google.gwt.core.client.JavaScriptObject;
+
+public final class SkyAtmosphere extends JavaScriptObject
+{
+	protected SkyAtmosphere() {}
+	
+	
+}
---
+++ src/main/java/org/cesiumjs/cesium/SkyBox.java
@@ -0,0 +1,41 @@
+package org.cesiumjs.cesium;
+
+import com.google.gwt.core.client.JavaScriptObject;
+import com.google.gwt.dom.client.ImageElement;
+
+public class SkyBox extends JavaScriptObject
+{
+	protected SkyBox() {}
+	
+	public static final native SkyBox create(String px, String nx, String py, String ny, String pz, String nz) /*-{
+		var skyBox = new Cesium.SkyBox({
+			positiveX : px,
+		    negativeX : nx,
+		    positiveY : py,
+		    negativeY : ny,
+		    positiveZ : pz,
+		    negativeZ : nz
+		});
+		return skyBox;
+	}-*/;
+	
+	public static final native SkyBox create(ImageElement px, ImageElement nx, ImageElement py, ImageElement ny, ImageElement pz, ImageElement nz) /*-{
+		var skyBox = new Cesium.SkyBox({
+			positiveX : px,
+		    negativeX : nx,
+		    positiveY : py,
+		    negativeY : ny,
+		    positiveZ : pz,
+		    negativeZ : nz
+		});
+		return skyBox;
+	}-*/;
+	
+	public final native SkyBox show(boolean show) /*-{
+		this.show = show;
+		return this;
+	}-*/;
+	
+
+
+}
