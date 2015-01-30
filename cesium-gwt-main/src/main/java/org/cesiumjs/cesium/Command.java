8a9aa01fdb36c3ee9868499f707ad937cf2c7f32
Jason Stanley <jason.w.stanley@leidos.com>
2015-01-30 08:20:19 -0500

    made protected constructors

--- cesium-gwt-main/src/main/java/org/cesiumjs/cesium/Command.java
+++ cesium-gwt-main/src/main/java/org/cesiumjs/cesium/Command.java
@@ -4,7 +4,7 @@ import com.google.gwt.core.client.JavaScriptObject;
 
 public class Command extends JavaScriptObject {
 
-	public Command(){}
+	protected Command(){}
 	
 	public final native static Command create()/*-{
       return new Cesium.Command(container, options)
